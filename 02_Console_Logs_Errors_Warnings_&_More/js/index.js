7a75752f2380040d2ab548d0db85b1c4dfb2bb0d
Mahmud <mdmahmudul413@gmail.com>
2022-08-29 09:50:17 +0600

    part 2 done

---
+++ 02_Console_Logs_Errors_Warnings_&_More/js/index.js
@@ -0,0 +1,49 @@
+console.log('Hello');
+console.log(32+2)
+console.log(true);
+console.log([12,14,14]);
+console.log({one: 1,
+two: 2, three: 3});
+
+// tabular form 
+console.table({harry: 32, code: 43, peter: 53})
+
+// to show warning message
+console.warn('This is a warning!!');
+
+// to show error message
+console.error('This is an error!!');
+
+
+
+
+// to see the execution time
+
+// execution time will count from here
+console.time('execution time');
+console.log('Hello');
+console.log(32+2)
+console.log(true);
+console.log([12,14,14]);
+console.log({one: 1,
+two: 2, three: 3});
+
+// tabular form 
+console.table({harry: 32, code: 43, peter: 53})
+
+// to show warning message
+console.warn('This is a warning!!');
+
+// to show error message
+console.error('This is an error!!');
+
+// execution time will count upto here
+console.timeEnd('execution time');
+
+console.assert(181 < 180, 'age > 180 is not possible');
+
+
+// to clear the console
+// console.clear();
+
+
