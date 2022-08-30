c31d2067aab73e91fa7fa00e09b713d20266fcb2
Mahmud <mdmahmudul413@gmail.com>
2022-08-30 10:29:57 +0600

    Data types in javascript

---
+++ 04_Data_Types_in_JavaScript/index.js
@@ -0,0 +1,60 @@
+// Premitive Data types
+// String 
+console.log('Hello');
+
+// String concatenation
+let name = 'Muhammad';
+console.log('Hello' + ' ' + name);
+
+// typeof return the data type
+console.log('Data type is ' + typeof(name));
+
+// Number 
+let age = 18;
+console.log(age);
+console.log('Data type is ' + typeof age);
+
+// Boolean 
+let isDriver = true;
+console.log(isDriver);
+console.log('Data type is ' + typeof isDriver);
+
+// null 
+let nullVar = null;
+console.log(nullVar);
+console.log('Data type is ' + typeof nullVar);
+
+// null 
+let undef = undefined;
+console.log(undef);
+console.log('Data type is ' + typeof undef);
+
+// Reference Data types
+
+// Array
+let arr = [1, 2, 3, false, 'moon', [1, 11, 111], {moon: 27, mahadi: 8}];
+console.log(arr);
+console.log('Data type is ' + typeof arr);
+
+// Object Literals
+let mathMarks = {
+    Rohan: 33,
+    'Harry Potter': 86,
+    sam: 67
+};
+console.log(mathMarks);
+console.log('Data type is ' + typeof mathMarks);
+
+
+// Function
+function findName() {
+    
+}
+console.log(findName);
+console.log('Data type is ' + typeof findName);
+
+// Function
+let date = new Date();
+console.log(date);
+console.log('Data type is ' + typeof date);
+
