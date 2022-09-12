24e288794233857e76c584b753ba68059845c68b
Mahmud <mdmahmudul413@gmail.com>
2022-09-12 09:47:07 +0600

    complete window object and understanding dom

---
+++ 11_Manipulating_Websites_Using_JS_Window_Object/index.js
@@ -0,0 +1,46 @@
+// DOM = Document Object Model
+let a = window;
+console.log(a);
+// window object is a global object in the javascript. so that, if we want to use alert function or any other function than, we can write alert() or window.alert(). Both are same.
+
+// window.alert('Hello Mahmud');
+// alert('Hello Mahmud');
+
+// prompt()
+// let b = prompt('Enter your name');
+// console.log(b);
+
+
+// confirm() : It will return true or false.
+// let c = confirm('Are you sure that you want to delete this message?');
+// console.log(c);
+
+// document 
+let d = document;
+console.log(d);
+
+// window.innerHeight and window.innerWidth
+let e = window.innerHeight;
+let f = window.innerWidth;
+console.log(e);
+console.log(f);
+
+// scrollX and scrollY to know the messurement of scrolling
+let g = scrollX;
+console.log(g)
+let h = scrollY;
+console.log(h)
+
+// window.location
+let i = location;
+console.log(i);
+
+// location.reload() will reload the tab
+// location.href will show the link
+// location.href = '//youtube.com' will load youtube
+console.log(location.toString());//will return the link
+
+// window.history
+let j = history;
+console.log(j);
+// history.go(1) will forward and history.go(-1) will backward the page
---
+++ 12_Understanding_DOM_&_Creating_a_Website_Layout/index.js
@@ -0,0 +1,36 @@
+console.log('Hello Guys.......!!!');
+
+// It will represent the DOM
+let a = document;
+console.log(a);
+console.log(typeof(a));
+//we can manupulate the DOM using javascript
+
+// to see the html collections document.all
+let b = document.all;
+console.log(typeof(b))//b is not an object
+
+
+// to select the single part of the dom
+// let b = document.head;
+// b = document.body;
+// b = document.title;
+// b = document.forms;
+// console.log(b);
+// console.log(b[0]);
+// console.log(b[1]);
+
+// to print the to elements of the object a using foreach loop
+// Array.from(b).forEach(function(element){
+//     console.log(element);
+// });
+
+// to see the links of the dom
+let c = document.links;
+console.log(c);
+console.log(c[0]);
+console.log(c[1]);
+console.log(c[2]);
+
+let d = c[3].href;
+console.log(d);
