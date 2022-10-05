185f29bdd55dbedb0660b0a7720cea6640ca8a5e
Mahmud <mdmahmudul413@gmail.com>
2022-10-05 09:44:16 +0600

    Library Project Done

--- 33_Project_2_Building_a_College_Library_Website/js/appEs6.js
+++ 33_Project_2_Building_a_College_Library_Website/js/appEs6.js
@@ -1,62 +1,58 @@
-console.log('Hello');
-
-// constructor
-function Book(name, author, type) {
-    this.name = name;
-    this.author = author;
-    this.type = type;
+class Book{
+    constructor (name, author, type) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
 }
 
-// Display Constructor
-function Display() {
+class Display{
+    // Add methods to display class
+    add(book) {
+        // console.log('Adding to UI');
+        let tableBody = document.getElementById('tableBody');
+        let uiString = `
+            <tr>
+                <td>${book.name}</td>
+                <td>${book.author}</td>
+                <td>${book.type}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += uiString;
+    }
 
-}
-
-// Add methods to display prototype
-Display.prototype.add = function (book) {
-    // console.log('Adding to UI');
-    let tableBody = document.getElementById('tableBody');
-    let uiString = `
-        <tr>
-            <td>${book.name}</td>
-            <td>${book.author}</td>
-            <td>${book.type}</td>
-        </tr>
-    `;
-    tableBody.innerHTML += uiString;
-}
-
-// to reset the form field
-Display.prototype.clear = function () {
-    let libraryForm = document.getElementById('libraryForm');
     // to reset the form field
-    libraryForm.reset();
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        // to reset the form field
+        libraryForm.reset();
 
-}
-
-// to validate the form field data
-Display.prototype.validate = function (book) {
-    if (book.name.legth < 2 || book.author.length < 2) {
-        return false;
-    }
-    else {
-        return true;
     }
 
-}
+    // to validate the form field data
+    validate(book) {
+        if (book.name.legth < 2 || book.author.length < 2) {
+            return false;
+        }
+        else {
+            return true;
+        }
 
-// show alert functionality
-Display.prototype.show = function (type, message) {
-    let alertMessage = document.getElementById('message');
-    alertMessage.innerHTML = `
-    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
-        <strong>Message:</strong> ${message}.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    `;
-    setTimeout(() => {
-        alertMessage.innerHTML = '';
-    }, 3000);
+    }
+
+    // show alert functionality
+    show(type, message) {
+        let alertMessage = document.getElementById('message');
+        alertMessage.innerHTML = `
+        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+            <strong>Message:</strong> ${message}.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `;
+        setTimeout(() => {
+            alertMessage.innerHTML = '';
+        }, 3000);
+    }
 }
 
 // Add submit event listener to form name libraryForm
