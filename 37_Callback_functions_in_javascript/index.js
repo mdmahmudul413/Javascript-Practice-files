6154642a09f9ed384d797cdaefeb8d6a37a6837b
Mahmud <mdmahmudul413@gmail.com>
2022-10-06 19:36:08 +0600

    Callback functions

---
+++ 37_Callback_functions_in_javascript/index.js
@@ -0,0 +1,41 @@
+console.log('Hello');
+
+// pretend that this response is coming from server
+const students = [
+    {name: 'harry', subject: 'javascript'},
+    {name: 'Rohan', subject: 'Machine Learning'}
+]
+
+// when a function take functions as argument, those functions are called call back functions.
+
+// function enrollStudent()
+function enrollStudent(student, callback){
+    setTimeout(function() {
+        students.push(student);
+        console.log('Student has been enrolled');
+        callback();
+    }, 3000);
+}
+// enrollStudent function take 8 sec to enroll a student
+
+// function getStudents()
+function getStudents(){
+    setTimeout(function() {
+        let str = '';
+        students.forEach(function(element) {
+            str += `<li>${element.name}</li>`;
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log('Student has been fetched');
+    }, 1000);
+}
+// getStudents function take 1 sec to fetch student data
+// enrollStudent({name:'Moon', Subject: 'Python'});
+// because of student enrollment take long time, that's why we don't see the data of 'Moon' in the DOM. that's why we have to set another callback parameter to enrollStudent to hold the execution of getStudents() function. and will not use direct call like 'getStudents()'
+ 
+// getStudents();
+
+// new way of enrollment using callback
+let moon = {name:'Moon', Subject: 'Python'};
+enrollStudent(moon, getStudents);
+
