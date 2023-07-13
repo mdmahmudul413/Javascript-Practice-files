8790d985255cc04c60cfbcce41d3b46f40baae67
Mahmud <mdmahmudul413@gmail.com>
2023-07-13 15:25:57 +0600

    all set

--- 02_Console_Logs_Errors_Warnings_&_More/js/index.js
+++ 02_Console_Logs_Errors_Warnings_&_More/js/index.js
@@ -1,3 +1,4 @@
+// to print something in the console
 console.log('Hello');
 console.log(32+2)
 console.log(true);
@@ -5,41 +6,33 @@ console.log([12,14,14]);
 console.log({one: 1,
 two: 2, three: 3});
 
-// tabular form 
+
+// print something in tabular form 
 console.table({harry: 32, code: 43, peter: 53})
+
 
 // to show warning message
 console.warn('This is a warning!!');
+
 
 // to show error message
 console.error('This is an error!!');
 
 
-
-
-// to see the execution time
+// ======================================
+//      To see the execution time
+// ======================================
 
 // execution time will count from here
 console.time('execution time');
-console.log('Hello');
-console.log(32+2)
-console.log(true);
-console.log([12,14,14]);
-console.log({one: 1,
-two: 2, three: 3});
 
-// tabular form 
-console.table({harry: 32, code: 43, peter: 53})
-
-// to show warning message
-console.warn('This is a warning!!');
-
-// to show error message
-console.error('This is an error!!');
+// the execution time of bellow this line will print
+console.log('hi');
 
 // execution time will count upto here
 console.timeEnd('execution time');
 
+// if condition is false, it will throw error in the console
 console.assert(181 < 180, 'age > 180 is not possible');
 
 
---
+++ readme.md
@@ -0,0 +1,44 @@
+## 02 Console Logs Errors Warnings & More
+
+    // to print something in the console
+    console.log('Hello');
+    console.log(32+2)
+    console.log(true);
+    console.log([12,14,14]);
+    console.log({one: 1,
+    two: 2, three: 3});
+
+
+    // print something in tabular form 
+    console.table({harry: 32, code: 43, peter: 53})
+
+
+    // to show warning message
+    console.warn('This is a warning!!');
+
+
+    // to show error message
+    console.error('This is an error!!');
+
+
+    // ======================================
+    //      To see the execution time
+    // ======================================
+
+    // execution time will count from here
+    console.time('execution time');
+
+    // the execution time of bellow this line will print
+    console.log('hi');
+
+    // execution time will count upto here
+    console.timeEnd('execution time');
+
+    // if condition is false, it will throw error in the console
+    console.assert(181 < 180, 'age > 180 is not possible');
+
+
+    // to clear the console
+    // console.clear();
+
+
